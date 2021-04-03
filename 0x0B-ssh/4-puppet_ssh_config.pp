121a016fd79128c8ec45c4ca73da2e22f92814fb
Justin4587 <Thurman@atlasok.com>
2021-04-03 18:44:31 -0500

    finally??

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -4,6 +4,6 @@ file { '/home/thurman/holberton-system_engineering-devops/0x0B-ssh':
   ensure => present
 }->
 file_line { 'Host_line':
-  line => 'Host',
-  path => '/home/thurman/holberton-system_engineering-devops/0x0B-ssh/2-ssh_config',
+  line   => 'Host',
+  path   => '/home/thurman/holberton-system_engineering-devops/0x0B-ssh/2-ssh_config',
 }
