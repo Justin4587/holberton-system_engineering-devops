98ce7a9081998601a1eee3b3c2bfda89fc3b6cf7
Justin4587 <Thurman@atlasok.com>
2021-05-19 22:27:56 -0500

    fucksticks is a real thing

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" We'll just leave a happy little comment right here """
+import requests
+
+
+def top_ten(subreddit):
+    """ give me your data """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'
+    head = {'User-Agent': 'This-doesnt-matter'}
+
+    try:
+        stuff = requests.get(url.format(subreddit), headers=head).json()
+        stuffplus = stuff.get('data').get('children')
+        for titles in stuffplus:
+            print(titles.get('data').get('title'))
+    except Exception:
+        return None
