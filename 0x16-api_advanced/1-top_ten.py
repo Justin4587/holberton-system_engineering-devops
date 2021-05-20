ac249fcd17a87201b07fd556eb3819487d418b40
Justin4587 <Thurman@atlasok.com>
2021-05-19 22:35:33 -0500

    fucksticks is a real thing

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ def top_ten(subreddit):
     head = {'User-Agent': 'This-doesnt-matter'}
 
     try:
-        stuff = requests.get(url.format(subreddit), headers=head).json()
+        stuff = requests.get(url.format(subreddit), headers=head, allow_redirects=False).json()
         stuffplus = stuff.get('data').get('children')
         for titles in stuffplus:
             print(titles.get('data').get('title'))
